6f9ede843253e09d01874da3e48b95fd3730d61e
Umeshkumar24 <dasumesh957@gmail.com>
2023-03-20 13:23:30 +0530

    all required datas are added

---
+++ app/src/main/java/com/dasumesh/uchat/Message.kt
@@ -0,0 +1,11 @@
+package com.dasumesh.uchat
+
+class Message {
+    var message: String? = null
+    var senderId:String? = null
+
+    constructor(message: String? , senderId:String?){
+        this.message = message
+        this.senderId = senderId
+    }
+}
